35e3898598dfd17257b719a0ca1a14c97bf51a64
Roman Chernobrivets <romanchernobr@gmail.com>
2024-03-10 19:20:45 +0000

    first commit

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,17 @@
+﻿// HelloWorld.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    float TF = 0.f;
+    std::cin >> TF;
+
+    const float TC = (TF - 32.f) * (5.f / 9.f);
+
+    std::cout << TF;
+    std::cout << " is equal to " << TC << " degrees" << std::endl;
+
+}
+
